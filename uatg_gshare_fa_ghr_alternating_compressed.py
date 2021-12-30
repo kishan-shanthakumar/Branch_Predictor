ec3d7bf4f6a5eb749fd6dc771e45129d97f495b7
KISHAN S <69047931+kishan-shanthakumar@users.noreply.github.com>
2021-12-30 19:14:15 +0530

    Update uatg_gshare_fa_ghr_alternating_compressed.py

--- uatg_gshare_fa_ghr_alternating_compressed.py
+++ uatg_gshare_fa_ghr_alternating_compressed.py
@@ -48,14 +48,14 @@ class uatg_gshare_fa_ghr_alternating_compressed(IPlugin):
         # register with alternating 0's and 1's pattern. eg. 010101010....
         # history_len = the size of the Global History Register (ghr) in bits.
         # By default history_len is set to be 8 bits.
-        # The generated assembly code will use the t0 register to alternatively
+        # The generated assembly code will use the x8 register to alternatively
         # enter and exit branches.
 
         # initial section in the ASM
         asm = ""
-        asm = asm + '\taddi t0,x0,1\n'
-        asm = asm + '\taddi t1,x0,1\n\taddi t2,x0,2\n\n'
-        asm = asm + '\tc.beqz  t0,lab0\n'
+        asm = asm + '\taddi x8,x0,1\n'
+        asm = asm + '\taddi x9,x0,1\n\taddi x10,x0,2\n\n'
+        asm = asm + '\tc.beqz  x8,lab0\n'
 
         # the assembly program is structured in a way that
         # there are odd number of labels.
@@ -66,18 +66,18 @@ class uatg_gshare_fa_ghr_alternating_compressed(IPlugin):
         for i in range(self._history_len):
             if i % 2:
                 asm = asm + 'lab' + str(i) + ':\n'
-                asm = asm + '\taddi t0,t0,1\n'
-                asm = asm + '\tc.beqz  t0,lab' + str(i + 1) + '\n'
+                asm = asm + '\taddi x8,x8,1\n'
+                asm = asm + '\tc.beqz  x8,lab' + str(i + 1) + '\n'
             else:
                 asm = asm + 'lab' + str(i) + ':\n'
-                asm = asm + '\taddi t0,t0,-1\n'
-                asm = asm + '\tc.beqz  t0,lab' + str(i + 1) + '\t\n'
+                asm = asm + '\taddi x8,x8,-1\n'
+                asm = asm + '\tc.beqz  x8,lab' + str(i + 1) + '\t\n'
 
         asm = asm + 'lab' + str(self._history_len) + ':\n'
-        asm = asm + '\taddi t0,t0,-1\n\n'
-        asm = asm + '\taddi t1,t1,-1\n\taddi t2,t2,-1\n'
-        asm = asm + '\tc.beqz  t1,lab0\n\taddi t0,t0,2\n'
-        asm = asm + '\tc.beqz  t2,lab0\n'
+        asm = asm + '\taddi x8,x8,-1\n\n'
+        asm = asm + '\taddi x9,x9,-1\n\taddi x10,x10,-1\n'
+        asm = asm + '\tc.beqz  x9,lab0\n\taddi x8,x8,2\n'
+        asm = asm + '\tc.beqz  x10,lab0\n'
 
         # compile macros for the test
         compile_macros = []
